1b5af43eb1d01b52afbdd9aba6d17971d056b91f
robertarnborg <robert.arnborg@gmail.com>
2021-01-05 13:34:49 +0100

    Added SaveManager and Checkpoints

---
+++ Assets/Rob/Rob Scripts/SaveSystem/SaveCheckPoint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class SaveCheckPoint : MonoBehaviour
+{
+    [SerializeField]
+    public string thisCheckPoint;
+
+    [NonSerialized]
+    private Vector3 _checkpointPosition;
+
+
+    private void Awake()
+    {
+        _checkpointPosition = transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // TO DO Add Tag or Id for Player Detection
+        SaveCurrentCheckpoint();
+    }
+
+    private void SaveCurrentCheckpoint()
+    {
+        SaveManager.Instance.SaveCurrentCheckpoint(_checkpointPosition);
+        Debug.Log("This Save Point = " + thisCheckPoint + " is Saved!");
+    }
+}
---
+++ Assets/Rob/Rob Scripts/SaveSystem/SaveManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SaveManager : MonoBehaviour
+{
+    private static SaveManager _instance;
+    public static SaveManager Instance { get { return _instance; } }
+
+    public GameObject player;
+
+    #region Save Data
+
+    public bool isPersistentDontDestroyOnLoad = true;
+
+    [SerializeField]
+    private Vector3 _currentCheckpointPosition;
+
+    private Scene _currentLevel;
+    #endregion
+    private void Awake()
+    {
+        if (isPersistentDontDestroyOnLoad)
+        {
+            DontDestroyOnLoad(this);
+        }
+
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
+        player = GameObject.FindWithTag("Player"); // Whatever player reference we need
+
+    }
+
+    private void Update()
+    {
+        // DEBUG
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    private void Start()
+    {
+        LoadCurrentCheckpoint();
+    }
+
+    public void SaveCurrentCheckpoint(Vector3 checkpointPosition)
+    {
+        _currentCheckpointPosition = checkpointPosition;
+    }
+
+    public void LoadCurrentCheckpoint()
+    {
+        if(player != null)
+        {
+            player.transform.position = _currentCheckpointPosition;
+        }
+    }
+
+}
